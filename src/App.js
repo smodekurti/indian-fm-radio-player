3631902ce6244c9bf5c7f7fedb7205c4294e821c
Vasu Modekurti <gitvasu@gmail.com>
2025-08-05 22:17:05 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RadioPlayer from './components/RadioPlayer';
+import './styles/globals.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RadioPlayer />
+    </div>
+  );
+}
+
+export default App;
